efb2d82602417c5a3da7abc8a9b46730a6afb7cf
Alexhwa <ah.alswad@gmail.com>
2021-10-01 19:58:28 -0500

    Piston

---
+++ LD2021/Assets/Scripts/Piston.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Piston : MonoBehaviour
+{
+
+    public float pushInterval;
+    public float pushDistance;
+    public float pushSpeed;
+    public float pullDelay;
+    private float originalY;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Push(pushInterval));
+        originalY = transform.localPosition.y;
+    }
+
+    private IEnumerator Push(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        transform.DOLocalMoveY(pushDistance, pushSpeed).SetEase(Ease.OutElastic);
+        StartCoroutine(Reset(pushSpeed + pullDelay));
+    }
+
+    private IEnumerator Reset(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        transform.DOLocalMoveY(originalY, pushSpeed).SetEase(Ease.OutCubic);
+        StartCoroutine(Push(pushInterval + pushSpeed));
+    }
+}

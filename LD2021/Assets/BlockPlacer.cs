10b69b338693c391a25602db05a6d8c8c8fd42d9
Matthew Zhang <matthew.zhang.306@gmail.com>
2021-10-01 20:51:42 -0500

    extremely basic thing where you click the block to select it and click somewhere in the middle to place it, also lwrp yay

---
+++ LD2021/Assets/BlockPlacer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockPlacer : MonoBehaviour
+{
+    public new Camera camera;
+
+    new Collider2D collider;
+    SpriteRenderer ghostRenderer;
+    SelectableBlock selectedBlock;
+
+    private void Start() {
+        ghostRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void OnEnable() {
+        SelectableBlock.OnSelect += OnSelectBlock;
+    }
+    private void OnDisable() {
+        SelectableBlock.OnSelect -= OnSelectBlock;
+    }
+
+
+    private void OnSelectBlock(SelectableBlock block) {
+        selectedBlock = block;
+        ghostRenderer.sprite = selectedBlock.GetComponent<SpriteRenderer>().sprite;
+    }
+
+
+    private void OnMouseDown() {
+        if (selectedBlock != null) {
+            selectedBlock.transform.position = ghostRenderer.transform.position;
+            selectedBlock.isLoaded = true;
+            selectedBlock = null;
+            ghostRenderer.sprite = null;
+        }
+    }
+
+    private void OnMouseOver() {
+        ghostRenderer.transform.position = camera.ScreenToWorldPoint(Input.mousePosition);
+        ghostRenderer.transform.position = new Vector3(Mathf.Round(ghostRenderer.transform.position.x), Mathf.Round(ghostRenderer.transform.position.y), 0);
+    }
+
+    private void OnMouseEnter() {
+        ghostRenderer.enabled = true;
+    }
+
+    private void OnMouseExit() {
+        ghostRenderer.enabled = false;
+    }
+}
---
+++ LD2021/Assets/SelectableBlock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectableBlock : MonoBehaviour
+{
+    public delegate void SelectableBlockDelegate(SelectableBlock block);
+    public static SelectableBlockDelegate OnSelect;
+
+    public bool isLoaded = false;
+    private new Collider2D collider;
+
+    private void Start() {
+        collider = GetComponent<Collider2D>();
+    }
+
+    private void OnMouseDown() {
+        if (isLoaded)
+            return;
+
+        OnSelect?.Invoke(this);
+    }
+}

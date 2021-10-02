d0c71ab31117a17736ec996c8d4d4c94f5f441a1
Matthew Zhang <matthew.zhang.306@gmail.com>
2021-10-02 15:53:20 -0500

    create twin block with spring joint between

---
+++ LD2021/Assets/Scripts/TwinBlock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwinBlock : MonoBehaviour
+{
+    public Transform block1;
+    public Transform block2;
+    LineRenderer lineRenderer;
+
+    private void Start() {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update() {
+        lineRenderer.SetPosition(0, block1.localPosition);
+        lineRenderer.SetPosition(1, block2.localPosition);   
+    }
+}
